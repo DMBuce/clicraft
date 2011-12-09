86bdbc2b9780b138796f2ab6086b3892fe8edbc0
Buce <dmbuce@gmail.com>
2011-12-08 23:24:29 -0600

    Move project-provided action scripts to lib
    Having major portions of the core code in etc/ could be confusing
    to users, who might mistakenly modify them directly rather. Rename
    those scripts as lib/action.d/*.sh and let them be overidden by
    etc/action.d/*.sh.

--- src/lib/functions.sh
+++ src/lib/functions.sh
@@ -4,10 +4,10 @@ action() {
 	ACTION="$1"
 	shift
 
-	if [ -f "$ETC/action.d/$ACTION-local.sh" ]; then
-		. "$ETC/action.d/$ACTION-local.sh" "$@"
-	elif [ -f "$ETC/action.d/$ACTION.sh" ]; then
+	if [ -f "$ETC/action.d/$ACTION.sh" ]; then
 		. "$ETC/action.d/$ACTION.sh" "$@"
+	elif [ -f "$LIB/action.d/$ACTION.sh" ]; then
+		. "$LIB/action.d/$ACTION.sh" "$@"
 	else
 		echo "$PROG: Unknown action: $ACTION" >&2
 	fi
