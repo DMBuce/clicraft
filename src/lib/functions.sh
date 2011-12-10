06f7398efa0241f5078355af50acd6a7982e4e01
Buce <dmbuce@gmail.com>
2011-12-09 18:20:51 -0600

    Add actionfile() function
    This refactors a portion of the action() function out into a separate
    actionfile() function, for getting the script that corresponds to a
    given action.

--- src/lib/functions.sh
+++ src/lib/functions.sh
@@ -1,15 +1,27 @@
 #!bash
 
+actionfile() {
+	ACTION="$1"
+	
+	if [ -f "$ETC/action.d/$ACTION.sh" ]; then
+		echo "$ETC/action.d/$ACTION.sh"
+	elif [ -f "$LIB/action.d/$ACTION.sh" ]; then
+		echo "$LIB/action.d/$ACTION.sh"
+	else
+		return 1
+	fi
+}
+
 action() {
 	ACTION="$1"
 	shift
 
-	if [ -f "$ETC/action.d/$ACTION.sh" ]; then
-		. "$ETC/action.d/$ACTION.sh" "$@"
-	elif [ -f "$LIB/action.d/$ACTION.sh" ]; then
-		. "$LIB/action.d/$ACTION.sh" "$@"
+	FILE=$(actionfile "$ACTION")
+	if [ $? = 0 ]; then
+		. "$FILE" "$@"
 	else
 		echo "$PROG: Unknown action: $ACTION" >&2
+		action help
 	fi
 }
 
