1d81ff76b7a828c790ff3149cfff146cc4989f73
Buce <dmbuce@gmail.com>
2012-01-24 16:56:16 -0600

    Example action scripts
    Add example scripts that can be installed to etc/action.d/ for
    extra functionality.
    
    Neither of these scripts have been extensively tested.

---
+++ scripts/explore.sh
@@ -0,0 +1,25 @@
+#!bash
+#
+# This script requires mcexplore.
+#
+# Copy to clicraft/etc/action.d/ to install.
+#
+# Usage: clicraft explore <size> [args]
+#
+#    Generates a rectangle of land of a given size. The <size> argument, as
+#    well as all additional <args>, get passed directly to mcexplore. See
+#    `mcexplore --help' for details.
+#
+
+# abort with an error if the server is already running
+if status; then
+	err "You cannot run this while $SERVER_NAME is running."
+	return 1
+fi
+
+if [ ! -f "$SERVER_JAR" ]; then
+	action init
+fi
+
+mcexplore -p "$SERVER_DIR" "$@"
+
---
+++ scripts/map.sh
@@ -0,0 +1,26 @@
+#!bash
+#
+# This script requires c10t.
+#
+# Copy to clicraft/etc/action.d/ to install.
+#
+# Usage: clicraft map [args]
+#
+#    Maps the world. Any additional <args> get passed to c10t. For valid
+#    <args>, see `c10t --help'.
+#
+
+# abort with an error if the server is already running
+if status; then
+	err "You cannot run this while $SERVER_NAME is running."
+	return 1
+fi
+
+MAP_DIR="$HOME/maps"
+LEVEL_NAME="$(serverprop 'level-name')"
+FILE="$MAP_DIR/$LEVEL_NAME-$(date +%Y%m%d).png"
+
+mkdir -p "$MAP_DIR"
+
+c10t -w "$SERVER_DIR/$LEVEL_NAME" -o "$FILE" "$@"
+
