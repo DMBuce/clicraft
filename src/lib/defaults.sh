fe816533bef824e1c797069778d9726e2978b6fe
Buce <dmbuce@gmail.com>
2012-02-07 17:22:52 -0600

    Drop SERVER_TYPE config
    Bukkit doesn't have a stable URI for its jar files, so we can't
    easily support automagically updating to the latest Recommended Builds.

--- src/lib/defaults.sh
+++ src/lib/defaults.sh
@@ -4,16 +4,8 @@ SERVER_DIR="${SERVER_DIR-$CLICRAFT/srv}"
 
 SERVER_NAME="${SERVER_NAME-minecraft}"
 
-case "$SERVER_TYPE" in
-  "bukkit")
-	SERVER_JAR="${SERVER_JAR-$SERVER_DIR/craftbukkit.jar}"
-	SERVER_URL="${SERVER_URL-http://ci.bukkit.org/job/dev-CraftBukkit/promotion/latest/Recommended/artifact/target/craftbukkit-0.0.1-SNAPSHOT.jar}"
-  ;;
-  "minecraft"|*)
-	SERVER_JAR="${SERVER_JAR-$SERVER_DIR/minecraft_server.jar}"
-	SERVER_URL="${SERVER_URL-https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar}"
-  ;;
-esac
+SERVER_JAR="${SERVER_JAR-$SERVER_DIR/minecraft_server.jar}"
+SERVER_URL="${SERVER_URL-https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar}"
 
 START_COMMAND="${START_COMMAND-java -jar '$SERVER_JAR' nogui}"
 DOWNLOAD_COMMAND="${DOWNLOAD_COMMAND-curl -# -o "$SERVER_JAR" "$SERVER_URL"}"
