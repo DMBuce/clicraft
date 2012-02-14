55bc5b790c5fa69bb9952ea29f1c21b48ac5ebd1
Buce <dmbuce@gmail.com>
2012-02-14 11:07:13 -0600

    Follow redirects
    Add -L to curl's default options so that it will follow redirects.

--- src/lib/defaults.sh
+++ src/lib/defaults.sh
@@ -8,5 +8,5 @@ SERVER_JAR="${SERVER_JAR-$SERVER_DIR/minecraft_server.jar}"
 SERVER_URL="${SERVER_URL-https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar}"
 
 START_COMMAND="${START_COMMAND-java -jar '$SERVER_JAR' nogui}"
-DOWNLOAD_COMMAND="${DOWNLOAD_COMMAND-curl -# -o "$SERVER_JAR" "$SERVER_URL"}"
+DOWNLOAD_COMMAND="${DOWNLOAD_COMMAND-curl -#L -o "$SERVER_JAR" "$SERVER_URL"}"
 
