ae5a56d4fb36c79c504db62c108088b0d5b988e3
Buce <dmbuce@gmail.com>
2012-02-14 11:32:16 -0600

    Revert "Drop SERVER_TYPE config"
    With the introduction of dl.bukkit.org, we can sanely use
    SERVER_TYPE again.
    
    This reverts commit fe816533bef824e1c797069778d9726e2978b6fe.

--- src/lib/defaults.sh
+++ src/lib/defaults.sh
@@ -4,8 +4,13 @@ SERVER_DIR="${SERVER_DIR-$CLICRAFT/srv}"
 
 SERVER_NAME="${SERVER_NAME-minecraft}"
 
-SERVER_JAR="${SERVER_JAR-$SERVER_DIR/minecraft_server.jar}"
-SERVER_URL="${SERVER_URL-https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar}"
+if [ "$SERVER_TYPE" = "bukkit" ]; then
+	SERVER_JAR="${SERVER_JAR-$SERVER_DIR/craftbukkit.jar}"
+	SERVER_URL="${SERVER_URL-http://cbukk.it/craftbukkit.jar}"
+else
+	SERVER_JAR="${SERVER_JAR-$SERVER_DIR/minecraft_server.jar}"
+	SERVER_URL="${SERVER_URL-https://s3.amazonaws.com/MinecraftDownload/launcher/minecraft_server.jar}"
+fi
 
 START_COMMAND="${START_COMMAND-java -jar '$SERVER_JAR' nogui}"
 DOWNLOAD_COMMAND="${DOWNLOAD_COMMAND-curl -#L -o "$SERVER_JAR" "$SERVER_URL"}"
