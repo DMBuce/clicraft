6eeae54ebc183c65e8b241e5106034cfee3cee05
Buce <dmbuce@gmail.com>
2012-03-16 23:02:05 -0500

    Check for write perms on dl
    Instead of chugging along if a download fails, bail out.

--- src/lib/action.d/dl.sh
+++ src/lib/action.d/dl.sh
@@ -5,6 +5,12 @@
 #    Downloads the server jar file.
 #
 
+JAR_DIR="$(dirname "$SERVER_JAR")"
+if [ ! -w "$JAR_DIR" ]; then
+	err "No write permissions in $JAR_DIR"
+	return 1
+fi
+
 if [ -f "$SERVER_JAR" ]; then
 	msg "Backing up server jar to $(basename $SERVER_JAR).ccback"
 	mv "$SERVER_JAR" "$SERVER_JAR.ccback" || return $?
