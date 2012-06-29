eb65e42eedef731ffd6cf4119994b677f1961f76
Buce <dmbuce@gmail.com>
2012-06-29 11:12:37 -0500

    Add screen support
    Add support for GNU Screen.

---
+++ src/lib/screen.sh
@@ -0,0 +1,21 @@
+#!bash
+
+multiplex_start() {
+	screen -d -m -S "$SERVER_NAME" -t "$SERVER_NAME" $START_COMMAND
+}
+
+multiplex_cmd() {
+	screen -S "$SERVER_NAME" -p "$SERVER_NAME" -X stuff "$1
+"
+}
+
+multiplex_console() {
+	exec screen -d -r "$SERVER_NAME" -p "$SERVER_NAME"
+}
+
+multiplex_status() {
+	screen -q -ls "$SERVER_NAME"
+	test $? = 10
+	return $?
+}
+
