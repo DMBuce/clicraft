17b7de9d917663fcef21bd24a72819190d1201a6
Buce <dmbuce@gmail.com>
2012-07-18 21:35:30 -0500

    Rewrite cmd.sh
    Modify the `clicraft cmd` action in the following ways:
    
    * Utilize redb_lookup() and serverlog() when possible to keep
      clicraft from returning before the command finishes
    * Add a --verbose option for printing server.log, and a --quiet
      option for toggling --verbose

--- src/lib/action.d/cmd.sh
+++ src/lib/action.d/cmd.sh
@@ -1,12 +1,57 @@
 #!bash
 #
-# Usage: clicraft cmd <command>
+# Usage: clicraft cmd [-q|-v] <command>
 #
 #    Sends <command> to the server console.
 #
 
-CMD="$*"
+VERBOSITY=0
 
-action status >/dev/null && \
-	multiplex_cmd "${CMD#/}"
+# parse options
+while [ "${1:0:1}" = '-' ]; do
+	case "$1" in
+	  "-q"|"--quiet")   VERBOSITY=0 ;;
+	  "-v"|"--verbose") VERBOSITY+=1 ;;
+	  "--") break ;;
+	  *)
+		action help "$ACTION"
+		return 1
+	  ;;
+	esac
+	shift
+done
+
+CMD="${*#/}"
+
+if [ $# = 0 ]; then
+	action help "$ACTION"
+	return 1
+fi
+
+action status >/dev/null
+retval=$?
+
+# bail if server isn't running
+if [ "$retval" != 0 ]; then
+	return $retval
+fi
+
+RE_TIMESTAMP="$(redb_lookup timestamp)"
+RE_PATTERN="$(redb_lookup "cmd/${CMD%% *}" "$RE_TIMESTAMP" 2>/dev/null)"
+
+if [ "$RE_PATTERN" != "" ]; then
+	if [ "$VERBOSITY" != 0 ]; then
+		serverlog "$RE_PATTERN" multiplex_cmd "$CMD"
+	else
+		serverlog "$RE_PATTERN" multiplex_cmd "$CMD" >/dev/null
+	fi
+else
+	if [ "$VERBOSITY" != 0 ]; then
+		# send command to the server and print server.log for 2 seconds
+		TIMEOUT=2 serverlog 1000 multiplex_cmd "$CMD"
+	else
+		# send command to the server
+		multiplex_cmd "$CMD"
+	fi
+fi
 
