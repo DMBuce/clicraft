6c707fa8c461ae95a33886c271141ae3ce07c308
Buce <dmbuce@gmail.com>
2013-09-19 12:45:52 -0500

    Add substition for DOWNLOAD_COMMAND
    Change `clicraft dl` so that any occurences of %v are subsituted
    with the first argument or SERVER_VERSION. Using a version of
    "release" or "snapshot" will substitute the corresponding vanilla
    version.

--- src/lib/action.d/dl.sh
+++ src/lib/action.d/dl.sh
@@ -1,9 +1,24 @@
 #!bash
 #
-# Usage: clicraft dl
+# Usage: clicraft dl [version]
 #
 #    Downloads the server jar file.
 #
+
+SERVER_VERSION="${1-$SERVER_VERSION}"
+
+# dump versions.json to stdout
+versions_json() {
+	local version_url='http://s3.amazonaws.com/Minecraft.Download/versions/versions.json'
+	if which curl &>/dev/null; then
+		curl -sL "$version_url"
+	elif which wget &>/dev/null; then
+		wget -q -O- "$version_url"
+	else
+		err "Can't find curl or wget, unable to download versions.json"
+		return 1
+	fi
+}
 
 JAR_DIR="$(dirname "$SERVER_JAR")"
 if [[ ! -w "$JAR_DIR" ]]; then
@@ -11,6 +26,17 @@ if [[ ! -w "$JAR_DIR" ]]; then
 	return 1
 fi
 
+# substitute version
+if grep -q '%v' <<<"$DOWNLOAD_COMMAND"; then
+	VERSION="$SERVER_VERSION"
+	if [[ "$SERVER_VERSION" == 'snapshot' || "$SERVER_VERSION" == 'release' ]]; then
+		VERSION="$(versions_json | awk -F\" "/\"$SERVER_VERSION\":/ {print \$4}")"
+		msg "Latest $SERVER_VERSION version is $VERSION"
+	fi
+	DOWNLOAD_COMMAND="${DOWNLOAD_COMMAND//'%v'/$VERSION}"
+fi
+
+# back up server jar
 if [[ -f "$SERVER_JAR" ]]; then
 	msg "Backing up server jar to $(basename $SERVER_JAR).ccback"
 	mv "$SERVER_JAR" "$SERVER_JAR.ccback" || return $?
