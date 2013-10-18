2934b3afe3548d4588b43ccc534283672aef5990
Buce <dmbuce@gmail.com>
2013-10-17 21:55:30 -0500

    Change `clicraft explore` usage
    Having to feed mcexplore options to this script isn't very useful,
    so make it accept the size in blocks and optionally the center
    coordinates instead.
    
    For more control over the mcexplore command that gets launched,
    users can invoke mcexplore directly or write their own action
    with `clicraft edit`.

--- scripts/explore.sh
+++ scripts/explore.sh
@@ -2,12 +2,24 @@
 #
 # This script requires mcexplore.
 #
-# Usage: clicraft explore <size> [args]
+# Usage: clicraft explore <size> [x z]
 #
-#    Generates a rectangle of land of a given <size> using mcexplore. By
-#    default, <size> is measured in chunks. Any additional <args> get passed to
-#    mcexplore. For valid <args>, see `mcexplore --help`.
+#    Generates a world roughly <size> by <size> centered on coordinates
+#    <x> and <z> (spawn by default) using mcexplore.
 #
+
+RADIUS="$(($1 / 16 + 1))"
+x="$2"
+z="$3"
+
+# validate args
+if [[ $# != 1 && $# != 3 ]]; then
+	action help explore2
+	return 1
+elif ! [[ "$1" =~ ^[0-9]+$ && "$2" =~ ^-?[0-9]*$ && "$3" =~ ^-?[0-9]*$ ]]; then
+	err "Arguments must be integers"
+	return 1
+fi
 
 # abort with an error if the server is already running
 if status; then
@@ -28,5 +40,9 @@ if [[ ! -f "$SERVER_JAR" ]]; then
 	action dl
 fi
 
-$MCEXPLORE -p "$SERVER_DIR" -c "$START_COMMAND" "$@"
+if [[ "$x" != "" && "$z" != "" ]]; then
+	$MCEXPLORE -p "$SERVER_DIR" -c "$START_COMMAND" -x "$x" -z "$z" "$RADIUS"
+else
+	$MCEXPLORE -p "$SERVER_DIR" -c "$START_COMMAND" "$RADIUS"
+fi
 
