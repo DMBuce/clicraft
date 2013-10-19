6e28b0d620857331e4732a2f00a88fa98640f4d7
Buce <dmbuce@gmail.com>
2013-10-19 11:35:28 -0500

    Replace CMD_STRIP_SLASH with CMD_LSTRIP
    Instead of optionally stripping the leading slash from commands,
    let the user specify an arbitrary string to strip.

--- src/lib/action.d/cmd.sh
+++ src/lib/action.d/cmd.sh
@@ -21,18 +21,7 @@ while [ "${1:0:1}" = '-' ]; do
 	shift
 done
 
-CMD="$*"
-case "$CMD_STRIP_SLASH" in
-  [Tt]rue|[Yy]es)
-	CMD="${CMD#/}"
-  ;;
-  [Ff]alse|[Nn]o)
-	: # do nothing
-  ;;
-  *)
-	warn "Unrecognized CMD_STRIP_SLASH value: $CMD_STRIP_SLASH"
-  ;;
-esac
+CMD="${*#$CMD_LSTRIP}"
 
 if [[ $# = 0 ]]; then
 	action help "$ACTION"
