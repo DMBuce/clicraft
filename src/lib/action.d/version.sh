737e80a7b8d33cc5427ab5537aabcb41ad9cb56d
Buce <dmbuce@gmail.com>
2013-10-30 20:17:15 -0500

    Update copyright

--- src/lib/action.d/version.sh
+++ src/lib/action.d/version.sh
@@ -6,6 +6,6 @@
 #
 
 msg "%s %s" "$PROG" "$VERSION"
-msg "Copyright (C) %s" "2011-2012 DMBuce <dmbuce@gmail.com>"
+msg "Copyright (C) %s" "2011-2013 DMBuce <dmbuce@gmail.com>"
 msg "Home page: %s" "<$URL>"
 
