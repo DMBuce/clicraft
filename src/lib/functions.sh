8506a30c3cb1cae82fb4eee968d45909d5023d19
Buce <dmbuce@gmail.com>
2014-01-22 18:04:45 -0600

    Fix msg(), warn(), err()
    Put each statement on its own line, don't interpret <message> as an
    option to printf, and update the docs for these functions.

--- src/lib/functions.sh
+++ src/lib/functions.sh
@@ -2,20 +2,26 @@
 
 # Prints an info message to stdout
 msg() {
-	local mesg=$1; shift
-	printf "${mesg}\n" "$@"
+	local mesg=$1
+	shift
+
+	printf -- "${mesg}\n" "$@"
 }
 
 # Prints a warning to stderr
 warn() {
-	local mesg=$1; shift
-	printf "${mesg}\n" "$@" >&2
+	local mesg=$1
+	shift
+
+	printf -- "${mesg}\n" "$@" >&2
 }
 
 # Prints an error to stderr
 err() {
-	local mesg=$1; shift
-	printf "${mesg}\n" "$@" >&2
+	local mesg=$1
+	shift
+
+	printf -- "${mesg}\n" "$@" >&2
 }
 
 # Finds the location of a system action script
