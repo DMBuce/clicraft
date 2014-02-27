ce3c7655bb7c19bdd462888caf08ef76b127e04e
Buce <dmbuce@gmail.com>
2014-02-26 18:09:19 -0600

    Search for the config dir in more places
    Implement several different ways to tell clicraft which directory
    it should use for its configuration. In order of precedence,
    clicraft now searches the following places for its configs:
    
    * The argument to the --config option
    * The $CLICRAFT_CONFIG environment variable
    * $XDG_CONFIG_HOME/foo, where 'foo' is how clicraft was invoked
    * $XDG_CONFIG_HOME/clicraft
    * $sysconfdir/clicraft
    
    This adds a few other things along the way as well, including a
    bootstrap.sh file for functions needed before functions.sh is read,
    and proper options parsing.

---
+++ src/lib/bootstrap.sh
@@ -0,0 +1,26 @@
+#!bash
+
+# Prints an info message to stdout
+msg() {
+	local mesg=$1
+	shift
+
+	printf -- "${mesg}\n" "$@"
+}
+
+# Prints a warning to stderr
+warn() {
+	local mesg=$1
+	shift
+
+	printf -- "${mesg}\n" "$@" >&2
+}
+
+# Prints an error to stderr
+err() {
+	local mesg=$1
+	shift
+
+	printf -- "${mesg}\n" "$@" >&2
+}
+
