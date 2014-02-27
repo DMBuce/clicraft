e45c463d40ad7a045ebea6f405b2cdf63ebc28ed
Buce <dmbuce@gmail.com>
2014-02-26 18:16:43 -0600

    Update help output
    Add options to the help output.

--- src/lib/action.d/help.sh
+++ src/lib/action.d/help.sh
@@ -8,15 +8,25 @@
 
 __usage() {
 	local ACTIONS=( $(actions) )
-	fmt <<EOF
-Usage: $PROG <action> [args]
+	fmt -s <<EOF
+Usage: $PROG [options] <action> [args]
 
    Performs some action for the minecraft server. <action> can be one of:
 
       ${ACTIONS[@]}
 
-   To get usage for a particular action, run \`$PROG help <action>'.
-   For bug reports, use <$BUGREPORT>.
+   Valid <options> are:
+
+      -c, --config=<config>   Use a different <config> directory.
+
+      -h, --help              Display this help message and exit.
+
+      -V, --version           Display the version and exit.
+
+      -x, --xtrace            Set bash's xtrace option for debugging.
+
+   To get usage for a particular action, run \`$PROG help <action>'. For
+   bug reports, use <$BUGREPORT>.
 
 EOF
 }
