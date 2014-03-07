e616a1e9a5b949da79ef6a14a4a56595b87c9465
Buce <dmbuce@gmail.com>
2014-03-07 17:46:49 -0600

    Add options to `clicraft restart`
    Add a --warn option to `clicraft restart` which broadcasts a
    "Server is restarting" message in chat for 15 seconds before
    restarting. Also add an example restart script with additional
    --if-empty and --if-crashed options, which only restart the server
    if nobody's online or if it's down, respectively.

--- src/lib/action.d/restart.sh
+++ src/lib/action.d/restart.sh
@@ -1,9 +1,53 @@
 #!bash
 #
-# Usage: clicraft restart
+# Usage: clicraft restart [-w|--warn]
 #
-#    Restarts the server.
+#    Restarts the server. With --warn, broadcasts a warning in chat first.
 #
+
+broadcast() {
+	cmd say "$*"
+	msg "$*"
+}
+
+# normalize options
+shortopts="w"
+longopts=("warn")
+if ! validopts "$shortopts" "${longopts[@]}" -- "$@"; then
+	action help "$ACTION"
+	return 1
+else
+	set -- "${RETVAL[@]}"
+fi
+unset RETVAL
+unset shortopts
+unset longopts
+
+# parse options
+WARN=0
+while [[ "$1" == -* ]]; do
+	case "$1" in
+	  -w|--warn)       WARN=1 ;;
+	  --)              shift; break ;;
+	esac
+	shift
+done
+
+if (( WARN )) && status; then
+	broadcast "Restarting in 15 seconds"
+	sleep 5
+	broadcast "Restarting in 10 seconds"
+	sleep 5
+	broadcast "Restarting in 5 seconds"
+	sleep 2
+	broadcast "3..."
+	sleep 1
+	broadcast "2..."
+	sleep 1
+	broadcast "1..."
+	sleep 1
+	cmd say "The server is going down NOW"
+fi
 
 action stop
 action start
