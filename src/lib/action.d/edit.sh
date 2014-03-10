502c16770cf47263ac9a7f13ad002eb0704c71a8
Buce <dmbuce@gmail.com>
2014-03-09 21:13:45 -0500

    Rewrite `clicraft edit`
    Remove the behavior where comments are ignored when checking
    whether to install a new action file.
    
    Use example action scripts as templates.
    
    If a file is empty after editing it, delete it.

--- src/lib/action.d/edit.sh
+++ src/lib/action.d/edit.sh
@@ -2,91 +2,113 @@
 #
 # Usage: clicraft edit [file|action]
 #
-#    Edit <file> in the server directory. If <file> does not exist and has a
-#    .dat extension, and if vinbt is installed, edit the first file of that name
-#    found anywhere within the world directory using vinbt. Otherwise, edit the
-#    action script for <action> instead. If an action script for <action> does
-#    not exist, create it with a useful template.
-#
-#    When no <file> or <action> is present, edit clicraft.conf.
+#    Without <file> or <action>, edit clicraft.conf. If <file> exists in the
+#    server directory, edit it. If vinbt is installed, <file> has a .dat
+#    extension, and it exists in the world directory, edit <file> with vinbt.
+#    Otherwise, edit the action script for <action>. If the script does not
+#    already exist, create it with an appropriate template.
 #
 #    This command launches EDITOR to edit files, or vi if the EDITOR variable
 #    is not defined in your environment.
 #
 
+# main starting point for script
+main() {
+	local file action template world vinbt nbtfile
+	file="$1"
+	action=
+	template=
+
+	# figure out the file to edit and the template to use
+	if [[ -z "$file" ]]; then
+		file="$CLICRAFT_CONFIG/clicraft.conf"
+		template="$CLICRAFT_CONFIG/clicraft-defaults.conf"
+	elif [[ -f "$SERVER_DIR/$file" ]]; then
+		file="$SERVER_DIR/$file"
+	elif [[ "$file" == *.dat ]] \
+		&& world="$(serverprop level-name 2>/dev/null)" \
+		&& vinbt="$(wickedwhich vinbt vinbt.sh 2>/dev/null)" \
+		&& nbtfile="$(find "$SERVER_DIR/$world" -name "$file" | head -n1)" \
+		&& [[ -n "$nbtfile" ]]
+	then
+		file="$nbtfile"
+		$vinbt "$file"
+		return $?
+	elif [[ -f "$CLICRAFT_CONFIG/action.d/${file%.sh}.sh.example" ]]; then
+		file="$CLICRAFT_CONFIG/action.d/${file%.sh}.sh"
+		template="$file.example"
+	elif sys_actionfile "${file%.sh}" &>/dev/null; then
+		action="${file%.sh}"
+		file="$CLICRAFT_CONFIG/action.d/$action.sh"
+		template="$CLICRAFT_CONFIG/action.d/action-override.sh.example"
+	else
+		action="${file%.sh}"
+		file="$CLICRAFT_CONFIG/action.d/$action.sh"
+		template="$CLICRAFT_CONFIG/action.d/action.sh.example"
+	fi
+
+	if [[ -n "$template" && -n "$action" ]]; then
+		edit_from_template "$file" "$template" "s/\\\$ACTION/$action/g"
+	elif [[ -n "$template" ]]; then
+		edit_from_template "$file" "$template"
+	else
+		edit_file "$file"
+	fi
+}
+
+# edit file using EDITOR
 edit_file() {
-	local EDITOR="${EDITOR-vi}"
-	$EDITOR "$1"
-}
+	local file fileorig
+	file="$1"
 
-# edit FILE created from SUBST applied to TEMPLATE
-edit_file_template() {
-	local TEMPLATE FILE SUBST
-	TEMPLATE="$1"
-	FILE="$2"
-	SUBST="$3"
+	${EDITOR-vi} "$file"
 
-	# if file exists, just edit it
-	# otherwise, create it from a template before editing
-	if [[ -f "$FILE" ]]; then
-		edit_file "$FILE"
-	else
-		if [[ -f "$TEMPLATE" ]]; then
-			sed "$SUBST" "$TEMPLATE" >>"$FILE"
-		else
-			warn "Template file %s not found" "$TEMPLATE"
-		fi
-
-		edit_file "$FILE"
-
-		# if any non-comment, non-whitespace changes were made
-		if [[ -f "$TEMPLATE" ]] && \
-		   diff -q <(grep "^[ 	]*[^ 	#]" "$FILE") \
-		           <(grep "^[ 	]*[^ 	#]" "$TEMPLATE" | sed "$SUBST") >/dev/null; then
-			warn "Aborting installation of %s: No changes made to template." "${FILE##*/}"
-			rm "$FILE"
-		else
-			msg "New file %s successfully installed." "${FILE##*/}"
-		fi
+	if [[ -f "$file" && ! -s "$file" ]]; then
+		warn "Removing empty file %s" "$file"
+		rm -f "$file"
 	fi
 }
 
-edit_action() {
-	local ACTION ACTIONFILE TEMPLATE
-	ACTION="$1"
+# edit file from template
+edit_from_template() {
+	local file template action
+	file="$1"
+	template="$2"
+	subst="$3"
 
-	if ACTIONFILE="$(local_actionfile "$ACTION" 2>/dev/null)"; then
-		edit_file "$ACTIONFILE"
-	else
-		ACTIONFILE="$CLICRAFT_CONFIG/action.d/$ACTION.sh"
+	# edit file if it already exists
+	if [[ -f "$file" ]]; then
+		edit_file "$file"
+		return $?
+	fi
 
-		# figure out which template to use
-		if sys_actionfile "$ACTION" &>/dev/null; then
-			TEMPLATE="$CLICRAFT_CONFIG/action.d/action-override.sh.example"
-		else
-			TEMPLATE="$CLICRAFT_CONFIG/action.d/action.sh.example"
+	# create file from template
+	if [[ -f "$template" ]]; then
+		# file shouldn't exist, so abort if it does
+		if ! ( set -o noclobber
+		       sed -r "$subst" "$template" >"$file" )
+		then
+			err "Unexpected error encountered. Aborting."
+			exit 1
 		fi
+	else
+		warn "Template file %s not found" "$template"
+	fi
 
-		edit_file_template "$TEMPLATE" "$ACTIONFILE" "s/\$ACTION/$ACTION/g"
+	edit_file "$file"
+	if [[ ! -f "$file" ]]; then
+		return $?
+	fi
+
+	if [[ -f "$template" ]] && \
+		diff -q "$file" <(sed -r "$subst" "$template") >/dev/null
+	then
+		warn "Aborting installation of %s: No changes made to template." "${file##*/}"
+		rm "$file"
+	else
+		msg "New file %s successfully installed." "${file##*/}"
 	fi
 }
 
-file="$1"
-world="$(serverprop level-name 2>/dev/null)"
-vinbt="$(wickedwhich vinbt vinbt.sh 2>/dev/null)"
-if [[ "$file" == "" ]]; then
-	# edit clicraft.conf
-	edit_file_template "$CLICRAFT_CONFIG/clicraft-defaults.conf" "$CLICRAFT_CONFIG/clicraft.conf"
-elif [[ -f "$SERVER_DIR/$file" ]]; then
-	edit_file "$SERVER_DIR/$file"
-elif [[ "$file" == *.dat && "$world" != "" && "$vinbt" != "" ]]; then
-	nbtfile="$(find "$SERVER_DIR/$world" -name "$file" | head -n1)"
-	if [[ "$nbtfile" != "" ]]; then
-		$vinbt "$nbtfile"
-	else
-		edit_action "$file"
-	fi
-else
-	edit_action "$file"
-fi
+main "$@"
 
